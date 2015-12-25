1ff8b637830423bf6a02b9e93117c1f171ebcdbe
barriquello <barriquello@gmail.com>
2015-12-25 15:19:26 -0200

    add monitor menu

--- Modules/monitor/monitor_menu.php
+++ Modules/monitor/monitor_menu.php
@@ -4,4 +4,4 @@
   bindtextdomain($domain, "Modules/monitor/locale");
   bind_textdomain_codeset($domain, 'UTF-8');
 
- // $menu_left[] = array('name'=> dgettext($domain, "Monitor"), 'path'=>"monitor/list" , 'session'=>"write", 'order' => 0 );
+  $menu_left[] = array('name'=> dgettext($domain, "Monitor"), 'path'=>"monitor/list" , 'session'=>"write", 'order' => 0 );
