729c33c59d37b1ddab93debcdeb445f1528ebb7f
barriquello <barriquello@gmail.com>
2016-02-29 09:09:40 -0300

    add support for pt_BR, disable menu for inputs

--- Theme/theme.php
+++ Theme/theme.php
@@ -17,7 +17,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Emoncms</title>
+        <title>Simon</title>
 
         <!-- Thanks to Baptiste Gaultier for the emoncms dial icon http://bit.ly/zXgScz -->
         <link rel="shortcut icon" href="<?php echo $path; ?>Theme/favicon.png" />
